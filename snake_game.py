a67b6e1e34e2a5f9a91c63bd32a16c5278efd9e8
spinners25 <swieradlucas@gmail.com>
2025-08-01 10:43:49 -0400

    Implemented the basic agent

--- snake_game.py
+++ snake_game.py
@@ -37,7 +37,7 @@ BLOCK_SIZE = 20
 SPEED = 20
 
 
-class SnakeGame:
+class SnakeGameAI:
 
     def __init__(self, w=640, h=480):
         self.w = w
@@ -96,7 +96,7 @@ class SnakeGame:
         # 3. check if game over
         reward = 0
         game_over = False
-        if self._is_collision() or self.frame_iteration > 100*len(self.snake):
+        if self.is_collision() or self.frame_iteration > 100*len(self.snake):
             game_over = True
             reward =- 10
             return reward, game_over, self.score
@@ -115,7 +115,7 @@ class SnakeGame:
         # 6. return game over and score
         return reward, game_over, self.score
 
-    def _is_collision(self, pt=None):
+    def is_collision(self, pt=None):
         if pt is None:
             pt = self.head
         # hits boundary
